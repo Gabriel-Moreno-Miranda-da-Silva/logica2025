9427e11e60e34964379bbc562e250c17406c484a
Gabriel-Moreno-Miranda-da-Silva <eddysite69@outlook.com>
2025-04-25 14:20:54 -0300

    Create beecrowd | 1002.c

---
+++ beecrowd | 1002.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+ 
+int main() {
+ 
+   double n;
+   double raio;
+   double area;
+   n = 3.14159;
+   scanf("%lf",&raio) ;
+   area = n*pow(raio, 2);
+   printf("A=%#.4lf\n",area);
+   
+    return 0;
+}
