bd0d5814e8811f8b6037d5a67101bc304e42a376
Gabriel-Moreno-Miranda-da-Silva <eddysite69@outlook.com>
2025-05-09 16:14:08 -0300

    Update beecrowd | 1176.c

--- beecrowd | 1176.c
+++ beecrowd | 1176.c
@@ -1,1 +1,22 @@
-a fazer
+#include <stdio.h>
+
+int main () {
+    
+    unsigned long long int Fib[61], F1, F2, Fn;
+    int T, x, i;
+    Fib [0] = 0;
+    Fib [1] = 1;
+    Fib [2] = 1;
+    for (F1=1,F2=1,i=3;i<61;i++){
+        Fn = F1 + F2;
+        Fib[i] = Fn;
+        F1 = F2;
+        F2 = Fn;
+    }
+    scanf("%d",&T);
+    for (i=0;i<T;i++){
+        scanf("%d\n",&x);
+        printf("Fib(%d) = %llu\n", x, Fib[x]);
+    }
+    return 0;
+}
