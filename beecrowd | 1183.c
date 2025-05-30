b5df6c024bdb9afededabeaba7a14de8a550909b
Gabriel-Moreno-Miranda-da-Silva <eddysite69@outlook.com>
2025-05-30 15:07:00 -0300

    Create beecrowd | 1183.c

---
+++ beecrowd | 1183.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main() {
+ 
+    double M[12][12];
+    int i, j;
+    double R;
+    R = 0;
+    char O[2];
+    
+    scanf("%s\n", O);
+    
+    for (j=0;j<=11;j++){
+        for (i=0;i<=11;i++){
+            scanf("%lf\n", &M[i][j]);
+        }
+    }
+    if(strcmp(O,"S")==0){
+        for(j=0;j<=10;j++){
+            for(i=j+1;i<=11;i++)
+                R += M[i][j];
+        }
+    }
+    if(strcmp(O,"M")==0){
+        for(j=0;j<=10;j++){
+            for(i=j+1;i<=11;i++)
+                R += M[i][j];
+        }
+        R = R/66;
+    }
+    printf("%.1lf\n", R);
+ 
+    return 0;
+}
