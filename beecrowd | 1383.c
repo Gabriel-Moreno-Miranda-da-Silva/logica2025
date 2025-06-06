2d6958d0dacd55e8e643cdbb2211fb089bc194b6
Gabriel-Moreno-Miranda-da-Silva <eddysite69@outlook.com>
2025-06-06 09:05:56 -0300

    Update and rename inconpleto beecrowd | 1383.c to beecrowd | 1383.c

---
+++ beecrowd | 1383.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+
+int Verificar(int vet[]) {
+    int i1, i2;
+    int temp;
+    for (i1 = 0; i1 < 9; i1++) {
+        temp = vet[i1];
+        for (i2 = 0; i2 < 9; i2++) {
+            if (i1 != i2 && temp == vet[i2]) {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+int main() {
+    int M[9][9];
+    int m[9];
+    int i, j, n, temp;
+    int valido;
+
+    scanf("%d", &n);
+    for (int N = 0; N < n; N++) {
+        for (i = 0; i < 9; i++) {
+            for (j = 0; j < 9; j++) {
+                scanf("%d", &M[i][j]);
+            }
+        }
+
+        valido = 1;
+
+        //Verificar Todas as linhas
+        for (i = 0; i < 9; i++) {
+            for (j = 0; j < 9; j++) {
+                m[j] = M[i][j];
+            }
+            if (Verificar(m)) {
+                valido = 0;
+            }
+        }
+
+        //Verificar Todas as Colunas
+        for (j = 0; j < 9; j++) {
+            for (i = 0; i < 9; i++) {
+                m[i] = M[i][j];
+            }
+            if (Verificar(m)) {
+                valido = 0;
+            }
+        }
+
+        //Verificar Todas as Matrizes 3x3
+        for (int lin = 0; lin < 9; lin += 3) {
+            for (int col = 0; col < 9; col += 3) {
+                int k = 0;
+                for (i = lin; i < lin + 3; i++) {
+                    for (j = col; j < col + 3; j++) {
+                        m[k++] = M[i][j];
+                    }
+                }
+                if (Verificar(m)) {
+                    valido = 0;
+                }
+            }
+        }
+
+        printf("Instancia %d\n", N + 1);
+        if (valido == 0) {
+            printf("NAO\n");
+        } else {
+            printf("SIM\n");
+        }
+        printf("\n");
+    }
+    return 0;
+}
