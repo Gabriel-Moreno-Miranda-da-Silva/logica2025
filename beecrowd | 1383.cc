4db4224f3436baa56549933fdc523de8ecb1bbcb
Gabriel-Moreno-Miranda-da-Silva <eddysite69@outlook.com>
2025-06-18 10:45:27 -0300

    Update beecrowd | 1383.cc

--- beecrowd | 1383.cc
+++ beecrowd | 1383.cc
@@ -16,11 +16,49 @@ int Verificar(int vet[]) {
     }
     return 0;
 }
+int VerificarMatrix(int vet1[], int vet2[][9], int *R) {
+    int i, j, n;
+    //Verificar Todas as linhas
+    for (i = 0; i < 9; i++) {
+        for (j = 0; j < 9; j++) {
+            vet1[j] = vet2[i][j];
+        }
+        if (Verificar(vet1)) {
+            *R = 0;
+        }
+    }
+
+    //Verificar Todas as Colunas
+    for (j = 0; j < 9; j++) {
+        for (i = 0; i < 9; i++) {
+            vet1[i] = vet2[i][j];
+        }
+        if (Verificar(vet1)) {
+            *R = 0;
+        }
+    }
+
+    //Verificar Todas as Matrizes 3x3
+    for (int lin = 0; lin < 9; lin += 3) {
+        for (int col = 0; col < 9; col += 3) {
+            int k = 0;
+            for (i = lin; i < lin + 3; i++) {
+                for (j = col; j < col + 3; j++) {
+                    vet1[k++] = vet2[i][j];
+                }
+            }
+            if (Verificar(vet1)) {
+                *R = 0;
+            }
+        }
+    }
+    return 0;
+}
 
 int main() {
     int M[9][9];
     int m[9];
-    int i, j, n, temp;
+    int i, j, n;
     int valido;
 
     cin >> n;
@@ -32,41 +70,7 @@ int main() {
         }
 
         valido = 1;
-
-        //Verificar Todas as linhas
-        for (i = 0; i < 9; i++) {
-            for (j = 0; j < 9; j++) {
-                m[j] = M[i][j];
-            }
-            if (Verificar(m)) {
-                valido = 0;
-            }
-        }
-
-        //Verificar Todas as Colunas
-        for (j = 0; j < 9; j++) {
-            for (i = 0; i < 9; i++) {
-                m[i] = M[i][j];
-            }
-            if (Verificar(m)) {
-                valido = 0;
-            }
-        }
-
-        //Verificar Todas as Matrizes 3x3
-        for (int lin = 0; lin < 9; lin += 3) {
-            for (int col = 0; col < 9; col += 3) {
-                int k = 0;
-                for (i = lin; i < lin + 3; i++) {
-                    for (j = col; j < col + 3; j++) {
-                        m[k++] = M[i][j];
-                    }
-                }
-                if (Verificar(m)) {
-                    valido = 0;
-                }
-            }
-        }
+        VerificarMatrix(m, M, &valido);
 
         cout << "Instancia " << N + 1 << "\n";
         if (valido == 0) {
